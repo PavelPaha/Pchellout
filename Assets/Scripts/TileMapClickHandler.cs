cb7448a8e1e1072aa4532f2f0abbcbf7022d65f2
Kurinaya_bulavka <69955792+PavelPaha@users.noreply.github.com>
2023-04-13 22:43:00 +0500

    Теперь можно добавлять объект на карту, но не отлсеживаются коллизии

---
+++ Assets/Scripts/TileMapClickHandler.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileMapClickHandler : MonoBehaviour
+{
+    public Tilemap tilemap; // Ссылка на Tilemap
+    public GameObject objectPrefab; // Ссылка на ваш объект префаб
+    public int Delta = 3;
+    void Update()
+    {
+        // Проверяем нажатие левой кнопки мыши
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Конвертируем позицию мыши на экране в мировую позицию
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // Определяем ячейку на Tilemap, на которую был сделан клик
+            Vector3Int cellPosition = tilemap.WorldToCell(worldPosition);
+            // Получаем позицию левого нижнего угла выбранной ячейки
+            Vector3 cellBottomLeftWorldPos = tilemap.CellToWorld(cellPosition);
+
+            // Создаем объект на позиции левого нижнего угла ячейки
+            GameObject instance = Instantiate(objectPrefab, cellBottomLeftWorldPos, Quaternion.identity);
+
+            // Устанавливаем позицию Z для объекта, чтобы избежать перекрытия с тайлами
+            // instance.transform.position = new Vector3(instance.transform.position.x, instance.transform.position.y, -1f);
+        }
+    }
+
+
+}

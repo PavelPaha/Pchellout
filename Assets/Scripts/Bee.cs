9d52a7b099dd4d39f9dd1421c6a61d3683f89f2c
AIM1rage <michael050100@gmail.com>
2023-04-13 20:49:25 +0500

    Добавил пчелку, идущую за курсором

---
+++ Assets/Scripts/Bee.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Bee : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private Camera mainCamera;
+
+    private SpriteRenderer _spriteRenderer;
+    private Vector2 _previousPosition;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _previousPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mousePosition - (Vector2)transform.position).normalized;
+
+        transform.position = Vector2.MoveTowards(transform.position, mousePosition, speed * Time.deltaTime);
+
+        UpdateAnimationDirection();
+        _previousPosition = transform.position;
+    }
+
+    private void UpdateAnimationDirection()
+    {
+        float movementDirection = ((Vector2)transform.position - _previousPosition).x;
+
+        if (movementDirection > 0.01f)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if (movementDirection < -0.01f)
+        {
+            _spriteRenderer.flipX = true;
+        }
+    }
+}

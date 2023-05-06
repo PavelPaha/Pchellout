da105ef94e3264132130b07211b310afc7a6b79e
Kurinaya_bulavka <69955792+PavelPaha@users.noreply.github.com>
2023-05-06 16:58:31 +0500

    Раскидал скрипты по папкам

--- Assets/Scripts/Bee/EnemiesSpawner.cs
+++ Assets/Scripts/Bee/EnemiesSpawner.cs
@@ -5,19 +5,6 @@ using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-/// <summary>
-/// Это класс волны нападения, который хранит параметры для врага,
-/// которого будет генерировать EnemiesSpawner
-/// </summary>
-public class AttackWave
-{
-    //TODO перенести этот класс в Locals
-    public int EnemyCount;
-    public float Duration;
-    public int Speed;
-    public float Scale = 1;
-    public int EnemyIndex;
-}
 
 /// <summary>
 /// Генерирует врагов (EnemyItem) рандомным образом
---
+++ Assets/Scripts/Global/AttackWave.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// Это класс волны нападения, который хранит параметры для врага,
+/// которого будет генерировать EnemiesSpawner
+/// </summary>
+public class AttackWave
+{
+    //TODO перенести этот класс в Locals
+    public int EnemyCount;
+    public float Duration;
+    public int Speed;
+    public float Scale = 1;
+    public int EnemyIndex;
+}

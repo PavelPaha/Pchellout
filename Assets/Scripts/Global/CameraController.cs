8453f518507bd2ce72a8b756c41b50618faeb24e
Kurinaya_bulavka <69955792+PavelPaha@users.noreply.github.com>
2023-06-02 02:07:57 +0500

    Адаптация камеры к разным разрешениям экрана

---
+++ Assets/Scripts/Global/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Global
+{
+    public class CameraController : MonoBehaviour
+    {
+        public float targetAspectRatio = 16f / 9f;
+
+        void Update()
+        {
+            float windowAspect = (float)Screen.width / (float)Screen.height;
+            float scaleHeight = windowAspect / targetAspectRatio;
+            Camera camera = GetComponent<Camera>();
+
+            if (scaleHeight < 1.0f)
+            {
+                Rect rect = camera.rect;
+                rect.width = 1.0f;
+                rect.height = scaleHeight;
+                rect.x = 0;
+                rect.y = (1.0f - scaleHeight) / 2.0f;
+                camera.rect = rect;
+            }
+            else
+            {
+                float scaleWidth = 1.0f / scaleHeight;
+                Rect rect = camera.rect;
+                rect.width = scaleWidth;
+                rect.height = 1.0f;
+                rect.x = (1.0f - scaleWidth) / 2.0f;
+                rect.y = 0;
+                camera.rect = rect;
+            }
+        }
+    }
+
+}
